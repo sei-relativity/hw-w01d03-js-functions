980f83b998fb395a9a672cfc01bb46c91007e72a
Lameesaf <lafallaj2@gmail.com>
2019-10-01 17:53:08 +0300

    donw with part1

---
+++ rps.js
@@ -0,0 +1,28 @@
+array = ['rock', 'paper', 'scissors']
+const playGame = function(choice) {
+    let comChoice = array[Math.floor(Math.random() * 2)]
+    if (comChoice === choice) {
+        return 'it\'s a tie'
+    } else {
+
+        switch (choice) {
+            case 'rock':
+                if (comChoice === 'paper') { return 'I win' } else { return 'You win' }
+                break;
+            case 'paper':
+                if (comChoice === 'scissors') { return 'I win' } else { return 'You win' }
+                break;
+            case 'scissors':
+                if (comChoice === 'rock') { return 'I win' } else { return 'You win' }
+                break;
+            default:
+                return 'COMON IS THIS YOUR FIRST TIME PLAYING THIS GAME';
+        }
+    }
+
+}
+
+console.log(playGame('rock'))
+console.log(playGame('scissors'))
+console.log(playGame('paper'))
+console.log(playGame('hi'))
