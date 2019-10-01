4e5e84953fac3579267f7c1035de6227a2fa5ef1
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-01 17:44:22 +0300

    player restriction

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
 const rps = function (pMove) {
+    if(pMove == "rock" || pMove == "paper" || pMove == "scissors") {
+
     let cMove = Math.floor(Math.random() * 3) + 1;
     if (cMove == 1) {
         cMove = "rock";
@@ -11,12 +13,15 @@ const rps = function (pMove) {
     console.log("You play " + pMove + ", Computer plays " + cMove);
     
 
-    if ((pMove == "rock" && cMove == "scissors") || (pMove == "paper" && cMove == "rock") ||(pMove == "scissors")){
+    if ((pMove == "rock" && cMove == "scissors") || (pMove == "paper" && cMove == "rock") ||(pMove == "scissors" && cMove =="paper")){
         return "player wins";
     } else if (pMove == cMove){
         return "It's a tie";
     } else {
         return "Computer wins";
     }   
+} else {
+    return "you didnt play a proper move";
+}
 
 }
