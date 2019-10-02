cc03a81e9973cc59ade1f4878015e7843b1c5d97
saera1988 <saera1988@hotmail.com>
2019-10-02 09:13:39 +0300

    game is done

---
+++ rps.js
@@ -0,0 +1,26 @@
+let userOp = prompt("Rock, Paper, or Scissors");
+ let compOp = Math.floor(Math.random()*3);
+
+    if (compOp === 0) {returne "Scissors";}
+    if (compOp === 1)  {returne= "Paper";}
+    if (compOp === 2)  {returne = "Rock";}
+
+ let playGame = function(userOp,compOp){
+
+     
+    let  r = "Rock";
+    let p = "Paper";
+    let s = "Scissors";
+    if(userOp === compOp){
+       return "Tie";
+    }else{
+        if(userOp===r && compOp===s || userOp===p && compOp===r || userOp===s && compOp===p)
+        {
+           return "user Wins!";
+        }else{
+           return "computer Wins!";
+        }
+    }
+};
+
+playGame(userOp,compOp);
