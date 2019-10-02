fd4518507534d85fd5ffc6749746476fff7446be
Abdullahjr <55585730+Abdullahjr@users.noreply.github.com>
2019-10-02 09:07:04 +0300

    .

---
+++ dice.js
@@ -0,0 +1,6 @@
+const rollDice = function  (){
+    const num1 = Math.floor(1+Math.random()*6);
+    const num2 = Math.floor(1+Math.random()*6);
+    if 
+}
+}
---
+++ rp.js
@@ -0,0 +1,18 @@
+
+const playgame = function play();
+    const userChoice = prompt(A, B, C)
+    const computerChoice = math.randon(A, B, C)
+    {
+    if (userChoice == A && computerChoice == B || userChoic == B && computerChoice == C || userChoice == C && computerChoice == A);
+        console.log("YOULOSE")
+}
+ {
+    elseif (userChoice == A && computerChoice == C || userChoic == B && computerChoice == A || userChoice == C && computerChoice == B);
+    console.log("YOUWIN")
+ }
+    {
+    if (userChoice == C && computerChoice == C || userChoic == B && computerChoice == B ||userChoice == A && computerChoice == A);
+    console.log("TIE")
+
+    }
+playgame(rock)
