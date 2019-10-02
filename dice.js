081c69864ca44ba6ce6f20fd71afe6b2b17c278a
OBAID ALQAHTANI <obal3588@colorado.edu>
2019-10-02 04:13:45 +0300

    rollDice fun

---
+++ dice.js
@@ -0,0 +1,13 @@
+const rollDice = function(roll) {
+    let i =0;
+    let random_num=[]
+    
+    while (i<=roll){
+        random_num[i]=Math.floor(Math.random() * 6) + 1;
+        i++
+    }
+    const add = (a, b) => a + b
+    const sum = random_num.reduce(add)
+
+    return   [random_num,sum]
+}
